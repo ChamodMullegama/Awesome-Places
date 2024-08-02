3be85f5f810ed5669204ea48099469ab3c4cae2f
Chamod Mullegama <149089781+ChamodMullegama@users.noreply.github.com>
2024-08-02 23:10:24 +0530

    add colors  to  app

---
+++ lib/utils/colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+final Color mainTextColor = const Color(0xff3B3636).withOpacity(0.75);
+const Color mainColor = Color(0xff9E00FF);
+const Color mainBlack = Colors.black;
+const Color mainWhite = Colors.white;
+
+const Color firstCatgeroycolor = Color(0xff8E8FFA);
+const Color seconedCatgeroycolor = Color(0xffC2D9FF);
+const Color thirddCatgeroycolor = Color(0xffFFE500);
+
+const Color mainNaturalWondersColor =  Color(0xff2EB200);
+final Color subNaturalWondersColor = const Color(0xff2EB200).withOpacity(0.15);
+
+const Color mainNightlife =  Color(0xffFF9900);
+final Color subNightlife = const Color(0xff2EB200).withOpacity(0.52);
+
+const Color mainLandmarks =  Color(0xff0029FF);
+final Color subLandmarks= const Color(0xff0029FF).withOpacity(0.66);
+final Color landmarksBackgroundColor = const Color(0xffCACACA).withOpacity(0.31);
+
+const Color mainCultural =  Color(0xffC00C00);
+
+const Color mainGreenColor =  Color(0xff06FFA5);
+const Color mainRedColor =  Color(0xffFF1E0F);
+
