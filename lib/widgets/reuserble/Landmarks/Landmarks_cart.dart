366f275c2b76aec19445b2776421ad1682bb3076
Chamod Mullegama <149089781+ChamodMullegama@users.noreply.github.com>
2024-08-23 21:15:00 +0530

    Update Landmarks_cart.dart

--- lib/widgets/reuserble/Landmarks/Landmarks_cart.dart
+++ lib/widgets/reuserble/Landmarks/Landmarks_cart.dart
@@ -25,7 +25,7 @@ class LandmarksCart extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(9.0),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(10),
               child: Image.asset(
